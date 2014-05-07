8b57e1389262b6b9d766c0f2db830edf445b7f43
Guillaume Vincent <vincent.guillaume.inp@gmail.com>
2014-05-07 13:35:47 +0200

    add ie compatibility with html5shiv and respond

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -12,11 +12,18 @@ var gulp = require('gulp'),
 
 var paths = {
     css: [
-        '../static/css/*.css',
+        '../static/css/style.css',
         '../static/lib/bootstrap/dist/css/bootstrap.css'
     ],
     images: [
         '../static/img/logo.png',
+    ],
+    ie_js: [
+        '../static/lib/html5shiv/dist/html5shiv.js',
+        '../static/lib/respond/src/respond.js'
+    ],
+    ie_css: [
+        '../static/css/ie.css'
     ]
 };
 
@@ -27,24 +34,38 @@ gulp.task('clean', function () {
     }).pipe(clean({force: true}));
 });
 
-gulp.task('css', ['fonts', 'images', 'clean'], function () {
-    gulp.src(paths.css)
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefix('last 2 versions'))
-        .pipe(gulp.dest('../public/css'));
-});
 
-
-gulp.task('fonts', function () {
+gulp.task('fonts', ['clean'], function () {
     gulp.src('../static/**/fonts/*')
         .pipe(flatten())
         .pipe(gulp.dest('../public/fonts/'));
 });
 
-gulp.task('images', function () {
+gulp.task('ie_js', ['fonts'], function () {
+    return gulp.src(paths.ie_js)
+        .pipe(concat('ie.min.js'))
+        .pipe(uglify({mangle: false}))
+        .pipe(gulp.dest('../public/js'))
+});
+
+gulp.task('ie_css', ['ie_js'], function () {
+    gulp.src(paths.ie_css)
+        .pipe(concat('ie.min.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(gulp.dest('../public/css'));
+});
+
+gulp.task('images', ['ie_css'], function () {
     return gulp.src(paths.images)
         .pipe(imagemin({optimizationLevel: 5}))
         .pipe(gulp.dest('../public/img'));
+});
+
+gulp.task('css', ['images'], function () {
+    gulp.src(paths.css)
+        .pipe(concat('style.min.css'))
+        .pipe(autoprefix('last 2 versions'))
+        .pipe(gulp.dest('../public/css'));
 });
 
 // watch files for changes
@@ -52,7 +73,7 @@ gulp.task('watch', function () {
     gulp.watch(paths.css, ['css']);
 });
 
-gulp.task('build', ['css', 'images'], function () {
+gulp.task('build', ['css'], function () {
     gulp.src('../public/css/style.min.css')
         .pipe(uncss({
             html: glob.sync('../_site/**/*.html')
